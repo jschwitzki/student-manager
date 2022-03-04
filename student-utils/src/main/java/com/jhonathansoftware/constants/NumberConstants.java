5cdc8bb6ba33be44720c6f584cba63f8141861f2
jschwitzki <jschwitzki@hotmail.com>
2022-03-03 21:45:15 -0300

    utils-folder

---
+++ student-utils/src/main/java/com/jhonathansoftware/constants/NumberConstants.java
@@ -0,0 +1,14 @@
+package com.jhonathansoftware.constants;
+
+public class NumberConstants {
+
+	private NumberConstants() {
+		
+	}
+	
+	public static final int FRAME_WIDTH = 850;
+	public static final int FRAME_HEIGHT = 600;
+	public static final int STUDENT_FORM_WIDTH = 400;
+	public static final int STUDENT_FORM_HEIGHT = 270;
+	public static final int REMOVE_FORM_HEIGHT = 170;
+}
